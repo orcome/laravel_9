09344a983d21dcf0ca4b786885b323c5c77ec831
Akhmad Herdian <akhmad.herdian@gmail.com>
2022-11-15 13:31:58 +0800

    Fixed form control on edit page

--- resources/views/masters/edit.blade.php
+++ resources/views/masters/edit.blade.php
@@ -19,7 +19,7 @@
                 <hr style="margin:0">
                 <div class="card-body text-danger">{{ __('master.delete_confirm') }}</div>
                 <div class="card-footer">
-                    <form method="POST" action="{{ route('masters.destroy', $master) }}" accept-charset="UTF-8" onsubmit="return confirm(&quot;{{ __('app.delete_confirm') }}&quot;)" class="del-form float-right" style="display: inline;">
+                    <form method="POST" action="{{ route('masters.destroy', $master) }}" accept-charset="UTF-8" onsubmit="return confirm(&quot;{{ __('app.delete_confirm') }}&quot;)" class="del-form float-end" style="display: inline;">
                         {{ csrf_field() }} {{ method_field('delete') }}
                         <input name="master_id" type="hidden" value="{{ $master->id }}">
                         <button type="submit" class="btn btn-danger">{{ __('app.delete_confirm_button') }}</button>
@@ -29,32 +29,33 @@
             </div>
         @endcan
         @else
-        <div class="card">
-            <div class="card-header">{{ __('master.edit') }}</div>
-            <form method="POST" action="{{ route('masters.update', $master) }}" accept-charset="UTF-8">
-                {{ csrf_field() }} {{ method_field('patch') }}
-                <div class="card-body">
-                    <div class="form-group">
-                        <label for="title" class="form-label">{{ __('master.title') }} <span class="form-required">*</span></label>
-                        <input id="title" type="text" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" name="title" value="{{ old('title', $master->title) }}" required>
-                        {!! $errors->first('title', '<span class="invalid-feedback" role="alert">:message</span>') !!}
+            <div class="card">
+                <div class="card-header">{{ __('master.edit') }}</div>
+                <form method="POST" action="{{ route('masters.update', $master) }}" accept-charset="UTF-8">
+                    {{ csrf_field() }} {{ method_field('patch') }}
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="title" class="form-label">{{ __('master.title') }} <span class="form-required">*</span></label>
+                            <input id="title" type="text" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" name="title" value="{{ old('title', $master->title) }}" required>
+                            {!! $errors->first('title', '<span class="invalid-feedback" role="alert">:message</span>') !!}
+                        </div>
+                        <div class="form-group">
+                            <label for="description" class="form-label">{{ __('master.description') }}</label>
+                            <textarea id="description" class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }}" name="description" rows="4">{{ old('description', $master->description) }}</textarea>
+                            {!! $errors->first('description', '<span class="invalid-feedback" role="alert">:message</span>') !!}
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="description" class="form-label">{{ __('master.description') }}</label>
-                        <textarea id="description" class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }}" name="description" rows="4">{{ old('description', $master->description) }}</textarea>
-                        {!! $errors->first('description', '<span class="invalid-feedback" role="alert">:message</span>') !!}
+                    <div class="card-footer">
+                        <input type="submit" value="{{ __('master.update') }}" class="btn btn-success">
+                        <a href="{{ route('masters.show', $master) }}" class="btn btn-link">{{ __('app.cancel') }}</a>
+                        @can('delete', $master)
+                            <a href="{{ route('masters.edit', [$master, 'action' => 'delete']) }}" id="del-master-{{ $master->id }}" class="btn btn-danger float-end">{{ __('app.delete') }}</a>
+                        @endcan
                     </div>
-                </div>
-                <div class="card-footer">
-                    <input type="submit" value="{{ __('master.update') }}" class="btn btn-success">
-                    <a href="{{ route('masters.show', $master) }}" class="btn btn-link">{{ __('app.cancel') }}</a>
-                    @can('delete', $master)
-                        <a href="{{ route('masters.edit', [$master, 'action' => 'delete']) }}" id="del-master-{{ $master->id }}" class="btn btn-danger float-right">{{ __('app.delete') }}</a>
-                    @endcan
-                </div>
-            </form>
-        </div>
+                </form>
+            </div>
+        @endif
     </div>
 </div>
-@endif
+
 @endsection
